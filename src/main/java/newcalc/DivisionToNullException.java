5d7b122cb851cafdd371e7ce6390a126a652ed10
umklajdet <pfedorov85@gmail.com>
2021-06-01 19:43:47 +0300

    реализовано здание 7

--- src/main/java/newcalc/DivisionToNullException.java
+++ src/main/java/newcalc/DivisionToNullException.java
@@ -1,2 +1,11 @@
-package newcalc;public class DivisionToNullException {
+package newcalc;
+
+public class DivisionToNullException extends Exception {
+    public DivisionToNullException() {
+    }
+
+    @Override
+    public String toString() {
+        return "DivisionToNullException {Division to null is impossible}";
+    }
 }
