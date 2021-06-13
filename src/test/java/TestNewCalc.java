eceb12ae7f5d1a007445e3fdadb58b8c5ba8710f
umklajdet <pfedorov85@gmail.com>
2021-06-13 15:58:35 +0300

    Задание 8: Созданы junit тесты по калькулятору

--- src/test/java/TestNewCalc.java
+++ src/test/java/TestNewCalc.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import java.awt.im.InputContext;
+import java.util.regex.Matcher;
 
 public class TestNewCalc {
 
@@ -21,25 +22,56 @@ public class TestNewCalc {
         Assert.assertTrue ("Multiplication to zero should be zero", op.getResult() == 0);
     }
 
-    @Test(expected = DivisionToNullException.class)
-    public void DivisionByZeroEx() throws DivisionToNullException {
-        Operation op = new Division(5, 0);
-        op.getResult();
+    @Test
+    public void AdditionBorderTest() {
+        Operation op = new Addition(4.9999999999, 10);
+        Assert.assertFalse("Result should be 14.9999999999", op.getResult() == 15);
     }
 
-    @Rule
-    public ExpectedException thrown = DivisionToNullException.none();
+    @Test
+    public void SomeAdditionResult(){
+        Operation op = new Addition(5, 6);
+        Assert.assertEquals("Result should be 11", 11, op.getResult(), 0.0);
+    }
+
+    @Test
+    public void SubtractionTest1(){
+        Operation op = new Subtraction(100.5, 0.5);
+        Assert.assertTrue("Result should be 100", op.getResult() == 100);
+    }
+
+    @Test
+    public void SubtractionTest2(){
+        Operation op = new Subtraction(1000.0000001, 0.0000001);
+        Assert.assertTrue("Result should be 1000", op.getResult() == 1000);
+    }
+
+    @Test
+    public void DivisionTest1(){
+        Operation op = new Division(10, 2.5);
+        Assert.assertEquals("Result should be 4", 4, op.getResult(), 0.0);
+    }
+
+    @Test
+    public void DivisionZeroTest() {
+        Operation op = new Division(0, 5);
+        Assert.assertEquals(0, op.getResult(), 0.0);
+    }
+
+//    @Test(expected = ArithmeticException.class)
+//    public void DivisionByZeroEx() throws ArithmeticException {
+//        Operation op = new Division(5, 0);
+//        op.getResult();*/
+//    }
+
+    /*@Rule
+    public ExpectedException thrown = DivisionToNullException();
     @Test
     public void DivisionByZeroExMsg() throws DivisionToNullException {
         Operation op = new Division(5, 0);
         thrown.expect(DivisionToNullException.class);
         thrown.expectMessage("DivisionToNullException {Division to null is impossible}");
         op.getResult();
-    }
+    }*/
 
-    @Test
-    public void SomeAdditionResult(){
-        Operation op = new Addition(5, 6);
-        Assert.assertTrue("Result should be 11", op.getResult() == 11);
-    }
 }
